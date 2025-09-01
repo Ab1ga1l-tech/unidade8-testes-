adc4ab3ac75612d76fd2871afe47b766bd11be24
Abigail Souza Santos <abi30souza@gmail.com>
2025-08-31 23:55:44 -0300

    novo comit

--- test/test_calc.py
+++ test/test_calc.py
@@ -1,4 +1,9 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
 from calc import soma
 
-def test_soma_basico():
+def test_soma():
     assert soma(2, 3) == 5
+    assert soma(-1, 1) == 0
+    assert soma(0, 0) == 0
